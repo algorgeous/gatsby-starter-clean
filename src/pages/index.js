258e8038554482af2e7964973c7f49144130a430
Alfred Bacon <bacon@gatech.edu>
2019-06-07 15:55:37 -0400

    Adding recipe listing link to index.

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,12 +9,11 @@ const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
     <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
+    <p>Welcome to my Drupal+Gatsby Recipe site.</p>
+    <Link to="/recipe-listing/">See the recipe listing</Link>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
-    <Link to="/page-2/">Go to page 2</Link>
   </Layout>
 )
 
